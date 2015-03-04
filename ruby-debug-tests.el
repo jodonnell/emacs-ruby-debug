629802517d5fcb292e1a79eeca73c5af28110965
Jacob O'Donnell <jacobodonnell@gmail.com>
2015-03-04 07:04:51 -0500

    advising the comint function rather than overwriting the process-filter

--- ruby-debug-tests.el
+++ ruby-debug-tests.el
@@ -1,5 +1,6 @@
 (setq test-doc
-"[12, 21] in /Users/jacob/programming/movie_matchmaker/app/controllers/welcome_controller.rb
+"
+[12, 21] in /Users/jacob/programming/movie_matchmaker/app/controllers/welcome_controller.rb
    12: 
    13:   def find_critics
    14:     byebug
@@ -11,6 +12,12 @@
    20: 
    21:   def delete_like_movie")
 
+(setq test-end-doc
+"(byebug) c
+  Rendered welcome/find_critics.html.erb within layouts/application (2.3ms)
+Completed 200 OK in 4822ms (Views: 439.7ms | ActiveRecord: 64.1ms)")
+
+
 
 (ert-deftest ruby-debug--get-current-line-from-output-test ()
   (should (= (ruby-debug--get-current-line-from-output test-doc) 17)))
@@ -18,3 +25,7 @@
 (ert-deftest ruby-debug--get-current-file-from-output-test ()
   (should (string= (ruby-debug--get-current-file-from-output test-doc)
                    "/Users/jacob/programming/movie_matchmaker/app/controllers/welcome_controller.rb")))
+
+(ert-deftest ruby-debug--is-debug-over-test ()
+  (should (ruby-debug--is-debug-over test-end-doc)))
+
--- ruby-debug.el
+++ ruby-debug.el
@@ -1,23 +1,38 @@
+(defvar ruby-debug--opened-buffers nil)
+
 (define-minor-mode ruby-debug-mode
   "Get your foos in the right places."
-  :lighter "ruby-debug"
+  :lighter " ruby-debug"
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map (kbd "n") 'ruby-debug-next-line)
+            (define-key map (kbd "c") 'ruby-debug-continue)
+            (define-key map (kbd "j") 'ruby-debug-jump)
             (define-key map (kbd "b") 'ruby-debug-add-fringe-breakpoint-to-list)
             map)
   (if (bound-and-true-p ruby-debug-mode)
-      (ruby-debug-move-current-line))
+      (message "hi"))
+                                        ;(set-process-filter (get-buffer-process "server") 'ruby-debug--process-filter))
   (ruby-debug-clear-overlay-arrows))
 
 (defun ruby-debug-move-line (line)
   (ruby-debug-add-fringe-at-line 'ruby-debug-current-line line))
 
+(defun ruby-debug-run-command(cmd)
+  (set-buffer "server")
+  (process-send-string (get-buffer-process "server") (concat cmd "\n")))
+
 (defun ruby-debug-next-line()
   (interactive)
-  (set-buffer "server")
-  (erase-buffer)
-  (process-send-string (get-buffer-process "server") "next\n"))
+  (ruby-debug-run-command "next"))
 
+(defun ruby-debug-continue()
+  (interactive)
+  (ruby-debug-run-command "continue"))
+
+(defun ruby-debug-jump()
+  (interactive)
+  (ruby-debug-add-breakpoint-at-current-line)
+  (ruby-debug-run-command "continue"))
 
 (defun ruby-debug-add-breakpoint()
   (interactive)
@@ -39,6 +54,8 @@
 (defun ruby-debug-clear-overlay-arrows ()
   (setq overlay-arrow-variable-list '()))
 
+(defun ruby-debug-clear-current-line-fringe ()
+  (setq overlay-arrow-variable-list (delete 'ruby-debug-current-line overlay-arrow-variable-list)))
 
 (defun ruby-debug-add-overlay-arrow (temp-var)
   (make-variable-buffer-local temp-var)
@@ -48,7 +65,7 @@
   (ruby-debug-add-overlay-arrow temp-var)
   (define-fringe-bitmap 'fringemark-hollow-right-arrow [128 192 96 48 24 48 96 192 128] 9 8 'center)
   (put temp-var 'overlay-arrow-bitmap 'fringemark-hollow-right-arrow)
-  (set temp-var (get-marker-at-beginning-of-line)))
+  (set temp-var (get-marker-at-beginning-of-line nil)))
 
 (defun ruby-debug-add-fringe-at-line (temp-var line)
   (ruby-debug-add-overlay-arrow temp-var)
@@ -56,8 +73,12 @@
   (set temp-var (get-marker-at-beginning-of-line line)))
 
 
+(defun ruby-debug-add-breakpoint-at-current-line ()
+  (ruby-debug-run-command (concat "b " (number-to-string (line-number-at-pos)))))
+
 (defun ruby-debug-add-fringe-breakpoint-to-list ()
   (interactive)
+  (ruby-debug-add-breakpoint-at-current-line)
   (let ((x 0)
         (symbol 'ruby-debug-breakpoint-mark0))
     (while (boundp symbol)
@@ -67,26 +88,39 @@
     (ruby-debug-add-fringe-breakpoint symbol)))
 
 
-
 (defun ruby-debug--get-current-line-from-output (output)
-  (if (string-match "=> +\\([0-9]+\\):" output)
+  (if (string-match "\n=> +\\([0-9]+\\):" output)
       (string-to-number (match-string 1 output))))
 
 (defun ruby-debug--get-current-file-from-output (output)
   (if (string-match "\n\[[0-9]+, [0-9]+\] in \\(.*\\)" output)
       (match-string 1 output)))
 
-(defun ruby-debug--process-filter(process output)
-  (set-buffer "server")
-  (insert output)
+(defun ruby-debug--is-debug-over (output)
+  (string-match "Completed [0-9]+" output))
+
+
+(defun ruby-debug--process-filter(output)
+  (ruby-debug-clear-current-line-fringe)
+  (ruby-debug--goto-debugged-line output)
+  (if (ruby-debug--is-debug-over output)
+      (ruby-debug--finish-debug)))
+
+(defun ruby-debug--goto-debugged-line (output)
   (let ((current-line (ruby-debug--get-current-line-from-output output))
         (filename (ruby-debug--get-current-file-from-output output)))
     (if (and current-line filename)
         (progn
           (find-file filename)
           (ruby-debug-move-line current-line)
-          (goto-line current-line)))))
+          (goto-line current-line)
+          (set-buffer "server")))))
 
-(set-process-filter (get-buffer-process "server") 'ruby-debug--process-filter)
+
+(defun ruby-debug--finish-debug ()
+  (ruby-debug-clear-overlay-arrows))
+;  (ruby-debug-mode))
 
 
+(add-hook 'comint-output-filter-functions 'ruby-debug--process-filter)
+;(remove-hook 'comint-output-filter-functions 'ruby-debug--process-filter)
